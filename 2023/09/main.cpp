5e81c3b521c98e1f265cc633d14ec9e843eac0ed
NMMN2 <125464473+NMMN2@users.noreply.github.com>
2023-12-10 12:05:48 +0000

    Day 9 part 2

--- 2023/09/main.cpp
+++ 2023/09/main.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 
 int predictNextValue(std::vector<int>& sequence);
+int predictPreviousValue(std::vector<int>& sequence);
 
 int main()
 {
@@ -41,6 +42,15 @@ int main()
 	}
 
 	std::cout << sumOfPredictions << "\n";
+
+	sumOfPredictions = 0;
+
+	for (std::vector<int> sequence : input)
+	{
+		sumOfPredictions += predictPreviousValue(sequence);
+	}
+
+	std::cout << sumOfPredictions << "\n";
 }
 
 
@@ -65,4 +75,27 @@ int predictNextValue(std::vector<int>& sequence)
 		return sequence.back();
 	else
 		return sequence.back() + predictNextValue(difference);
+}
+
+int predictPreviousValue(std::vector<int>& sequence)
+{
+	std::vector<int> difference;
+
+	for (int i = 0; i < sequence.size() - 1; i++)
+	{
+		difference.push_back(sequence[i + 1] - sequence[i]);
+	}
+
+	bool allZero = true;
+
+	for (int i : difference)
+	{
+		if (i != 0)
+			allZero = false;
+	}
+
+	if (allZero)
+		return sequence.front();
+	else
+		return sequence.front() - predictPreviousValue(difference);
 }
