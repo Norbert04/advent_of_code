12700f217c06486378e708e189e70f8ac02ee780
NMMN2 <125464473+NMMN2@users.noreply.github.com>
2023-12-10 11:47:42 +0000

    Day 9 part 1

---
+++ 2023/09/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+int predictNextValue(std::vector<int>& sequence);
+
+int main()
+{
+	std::vector <std::vector<int>> input;
+	std::ifstream file("input.txt");
+	if (file.is_open())
+	{
+		std::string line;
+		while (std::getline(file, line))
+		{
+			std::vector<int> history;
+			std::istringstream historyStream(line);
+			int temp;
+
+			while (historyStream >> temp)
+			{
+				history.push_back(temp);
+			}
+
+			input.push_back(history);
+		}
+		file.close();
+	}
+	else
+	{
+		return -1;
+	}
+
+	int sumOfPredictions = 0;
+
+	for (std::vector<int> sequence : input)
+	{
+		sumOfPredictions += predictNextValue(sequence);
+	}
+
+	std::cout << sumOfPredictions << "\n";
+}
+
+
+int predictNextValue(std::vector<int>& sequence)
+{
+	std::vector<int> difference;
+
+	for (int i = 0; i < sequence.size() - 1; i++)
+	{
+		difference.push_back(sequence[i + 1] - sequence[i]);
+	}
+
+	bool allZero = true;
+
+	for (int i : difference)
+	{
+		if (i != 0)
+			allZero = false;
+	}
+
+	if (allZero)
+		return sequence.back();
+	else
+		return sequence.back() + predictNextValue(difference);
+}
